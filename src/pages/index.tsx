192abbe4647f9213896c34115e10885719ea47d3
Juliane Pires <juliane.pires@lambda3.com.br>
2022-11-26 17:39:27 -0300

    feature - finaliza animação banner home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -42,7 +42,25 @@ export default function Home({
   }
 
   function handleScroll() {
-    AOS.init()
+    AOS.init({
+      // Global settings:
+      disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+      startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+      initClassName: 'aos-init', // class applied after initialization
+      animatedClassName: 'aos-animate', // class applied on animation
+      useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+      disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+      debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+      throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+      offset: 120, // offset (in px) from the original trigger point
+      delay: 0, // values from 0 to 3000, with step 50ms
+      duration: 800, // values from 0 to 3000, with step 50ms
+      easing: 'ease-in-out', // default easing for AOS animations
+      once: false, // whether animation should happen only once - while scrolling down
+      mirror: true, // whether elements should animate out while scrolling past them
+      anchorPlacement: 'top-center', // defines which position of the element regarding to window should trigger the animation
+    })
   }
 
   useEffect(() => {
@@ -59,18 +77,13 @@ export default function Home({
               alt="Logo Denise Ferreira"
               width={logoBranca.width}
               height={logoBranca.height}
-              data-aos="fade-right"
-              data-aos-duration="1000"
-              data-aos-offset="-200"
-              data-aos-easing="ease-in-sine"
-              data-aos-anchor="ancora-banner"
+              data-aos="fade-out"
+              data-aos-anchor="#ancora-banner"
             />
             <TextoDescricaoBanner
               data-aos="fade-right"
-              data-aos-anchor="ancora-banner"
-              data-aos-delay="200"
-              data-aos-easing="ease-in-sine"
-              data-aos-duration="1000">
+              data-aos-anchor="#ancora-banner"
+              data-aos-delay="200">
               {conteudoTexto.textoDescricaoBanner}
             </TextoDescricaoBanner>
           </Stack>
@@ -92,10 +105,8 @@ export default function Home({
             width={fotoDenise.width}
             height={fotoDenise.height}
             data-aos="fade-left"
-            data-aos-anchor="ancora-banner"
+            data-aos-anchor="#ancora-banner"
             data-aos-delay="300"
-            data-aos-easing="ease-in-sine"
-            data-aos-duration="1000"
           />
         </ContainerBanner>
       </ContainerConteudo>
